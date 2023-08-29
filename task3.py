9342636eea729c3e3a1648cea6ad32bbd069f34a
manurajusudheer <122018149+manurajusudheer@users.noreply.github.com>
2023-08-29 18:51:15 +0000

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,17 @@
+# Integer
+integer_var = 10
+float_var = 3.14
+string_var = "Hello, World!"
+boolean_var = True
+list_var = [1, 2, 3, 4, 5]
+tuple_var = (6, 7, 8, 9, 10)
+dictionary_var = {"name": "John", "age": 25, "city": "New York"}
+
+
+print("Integer:", integer_var)
+print("Float:", float_var)
+print("String:", string_var)
+print("Boolean:", boolean_var)
+print("List:", list_var)
+print("Tuple:", tuple_var)
+print("Dictionary:", dictionary_var)
